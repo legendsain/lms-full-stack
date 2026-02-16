8aa81d5742adfc8a92a5d76acc44da549e6b9859
legendsain <legendsain434@gmail.com>
2026-02-16 13:26:31 +0530

    fixed errors

--- server/routes/analyticsRoutes.js
+++ server/routes/analyticsRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { getAtRiskStudents } from '../controllers/analyticsController.js';
-import { requireAuth } from '../middlewares/authMiddleware.js';
+import { protectEducator } from '../middlewares/authMiddleware.js';
 
 const analyticsRouter = express.Router();
 
 // Protected route for educators
-analyticsRouter.get('/at-risk', requireAuth, getAtRiskStudents);
+analyticsRouter.get('/at-risk', protectEducator, getAtRiskStudents)
 
 export default analyticsRouter;
